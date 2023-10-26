679329156d01a6ce0f3d784ddf9c59b3a2c22afe
Yolonek <thejgrabowski@gmail.com>
2023-10-26 15:49:36 +0200

    finished ProbabilitySite description

--- ProbabilitySite.py
+++ ProbabilitySite.py
@@ -39,30 +39,10 @@ class ProbabilitySite(object):
         self.grid = self.grid.astype(int)
         self.step = 1
 
-    def plot_grid(self, save='', colormap=(), grid_threshold=0, initial=True):
-        if initial:
-            min_val, max_val = 0, 1
-        else:
-            min_val, max_val = np.min(self.grid), np.max(self.grid)
-        grid = self.initial_grid if initial else self.grid
-        if colormap:
-            colors = colormap[0]
-            quality = colormap[1]
-            cmap = LinearSegmentedColormap.from_list('', colors, N=quality)
-        else:
-            cmap = 'Greys'
-        if grid_threshold:
-            grid[grid >= grid_threshold] = grid_threshold
-        figure = plt.imshow(grid, cmap=cmap, vmin=min_val, vmax=max_val,
-                            interpolation='nearest')
-        if save:
-            pass
-        return figure
-
     def plot_grid_as_matrix(self, matrix=None, axes=None):
         if axes:
             axes.axis('off')
-            matrix = np.round(np.array(matrix if matrix else self.initial_grid), 4)
+            matrix = np.round(np.array(matrix if matrix is not None else self.initial_grid), 4)
 
             try:
                 cell_height = 1 / matrix.shape[0]
@@ -82,18 +62,19 @@ class ProbabilitySite(object):
                 cell.set_height(cell_height)
                 cell.set_width(cell_width)
 
-    def plot_grid2(self, initial=True, ax=None, title='', cmap_name='gray'):
+    def plot_grid(self, initial=True, ax=None, title='', cmap_name='gray'):
         if ax:
             grid = self.initial_grid if initial else self.grid
             ax.imshow(grid, cmap=cmap_name,
                       vmin=0, vmax=1, interpolation='nearest')
             if title:
                 ax.set_title(title)
+            ax.set(xticks=[], yticks=[])
 
 
 if __name__ == '__main__':
-    L = 100
-    p = [0.3, 0.5, 0.7]
+    L = 1000
+    p = [0.4, 0.6]
 
     figure, axes = plt.subplots(len(p), 2, layout='constrained')
     probability_grid = ProbabilitySite(L=L)
@@ -101,16 +82,15 @@ if __name__ == '__main__':
         probability_grid.change_probability(probability)
         probability_grid.grid_thresholding()
         title = f'$p = {probability}$'
-        probability_grid.plot_grid2(initial=True, ax=axes[index][0],
-                                    title=title, cmap_name='bone')
-        probability_grid.plot_grid2(initial=False, ax=axes[index][1],
-                                    title=title, cmap_name='bone')
-        axes[index][0].axis('off')
-        axes[index][1].axis('off')
+        probability_grid.plot_grid(initial=True, ax=axes[index][0],
+                                   title=title, cmap_name='bone')
+        probability_grid.plot_grid(initial=False, ax=axes[index][1],
+                                   title=title, cmap_name='bone')
 
-    figure.suptitle('Probability grid before and after thresholding for various $p$\n'
-                    'for the same probability grid')
-    figure.set_size_inches(5, 2.2 * len(p))
+    figure.suptitle(f'Probability grid before and after thresholding for various $p$\n'
+                    f'for the same probability grid\n'
+                    f'$L = {L}$')
+    figure.set_size_inches(7, 4 * len(p))
 
     image_path = 'images'
     make_directories([image_path])
